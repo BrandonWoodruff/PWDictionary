051facad5063b6f58e9624892f97aee1efc69830
BrandonWoodruff <bmw132000@gmail.com>
2024-04-04 15:01:58 -0600

    I have it working correctly

--- scrapePws.js
+++ scrapePws.js
@@ -1,56 +1,64 @@
 import puppeteer from "puppeteer"
 import fs from "fs"
+import progressbar from "progress";
+
+async function fail(errMsg,pageNum, pws, browser){
+  console.error(errMsg)
+  console.error(`page ${pageNum} failed`)
+  pws = pws.flat()
+  pws = pws.filter(pw => /^[a-zA-Z0-9]+$/.test(pw))
+  fs.writeFileSync('mcupws.json', JSON.stringify(pws, null, 2), 'utf-8')
+  await browser.close()
+  process.exit(1)
+}
 
 const main = async () => {
   // Launch the browser and open a new blank page
-  const pgStart = process.argv.slice(2)[0] || 1
+  const args = process.argv.slice(2)
+  const pgStart = Number(args[0]) || 1
   const browser = await puppeteer.launch({headness: 'new'})
   const page = await browser.newPage()
+  const bar = new progressbar("[:bar] :percent :etas", { total: 100 - pgStart + 1}); // TOTO use correct num
+  var timer = setInterval(function () {
+    if (bar.complete) {
+      clearInterval(timer);
+    }
+  }, 100);
 
   // Navigate the page to a URL
-  await page.goto("https://www.passwordrandom.com/most-popular-passwords");
+  await page.goto("https://www.passwordrandom.com/most-popular-passwords")
 
   // Set screen size
   await page.setViewport({ width: 1080, height: 1024 })
+  //ongoing sanity check, number of jws must be multiple of 100
   let pws = []
 
 
   for (let i = pgStart; i <= 100; i++) {
     try {
       pws.push(await page.evaluate(() => [...(document.querySelectorAll('td:nth-child(2)'))].map((td => td.innerText))))
+
     }
     catch (ignored) {
-      console.error(`page ${i} failed`)
-      pws = pws.flat()
-      fs.writeFileSync('mcupws.json', JSON.stringify(pws, null, 2), 'utf-8')
-      process.exit(1)
+      await fail(ignored, i, pws, browser)
     }
     await page.goto(`https://www.passwordrandom.com/most-popular-passwords?page=${i + 1}`)
-    console.log(`page ${i} done`)
+    // have it check the lengths of the arrays in pws
+    for (let j = 0; j < pws.length - 1; j++) {
+      if (pws[j].length !== 100) await fail(`mcupws.json had ${pws[j].length} words, consider deleting mcupws.json`, pgStart, pws, browser);
+    }
+    bar.tick()
   }
-
-
-
-  // Type into search box
-  // await page.type(".devsite-search-field", "automate beyond recorder")
-  // Wait and click on first result
-  // const searchResultSelector = ".devsite-result-item-link"
-  // await page.waitForSelector(searchResultSelector)
-  // await page.click(searchResultSelector)
-
-  // Locate the full title with a unique string
-  // const textSelector = await page.waitForSelector(
-  //   ".devsite-result-item-link"
-  // )
-  // const fullTitle = await textSelector?.evaluate((el) => el.textContent)
-  // const passwords = await textSelector?.evaluate((el) => el.innerText)
 
   // Print the full title
 
   await browser.close()
   //take out the multiple arrays inside pws
   pws = pws.flat()
+  //Use regEx to filter only alphanumeric passwords
+  pws = pws.filter(pw => /^[a-zA-Z0-9]+$/.test(pw))
   fs.writeFileSync('mcupws.json', JSON.stringify(pws, null, 2), 'utf-8')
+  console.log('SUCCESS! mcupws.json is good to go!')
 }
 
 main()
---
+++ spike.js
@@ -0,0 +1,22 @@
+//Spike One:
+
+// import progressbar from 'progress'
+
+// var bar = new progressbar('[:bar] :percent :etas', { total: 100 })
+
+// var timer = setInterval(function() {
+//   bar.tick()
+//   if (bar.complete) {
+//     clearInterval(timer)
+//   }
+// }, 100)
+
+// ---------------------------------------------------
+
+// Spike Two:
+const label = 'display update'
+console.time(label)
+for (let i = 0; i < 100_000; i++) {
+  if (i % 1000 === 0) console.timeLog(label, i)
+}
+console.timeEnd(label)
